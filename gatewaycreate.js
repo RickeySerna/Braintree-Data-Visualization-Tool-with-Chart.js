9240fadda871308e4e93a22ad133d6734035ea4c
RickeySerna <rickeyserna7@live.com>
2024-10-02 15:20:30 -0500

    Added Environment to .env template. Also added a .gitignore file.

--- gatewaycreate.js
+++ gatewaycreate.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const braintree = require('braintree');
 require('dotenv').config();
 
+const environmentMap = {
+  'Sandbox': braintree.Environment.Sandbox,
+  'Production': braintree.Environment.Production
+};
+
 const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
+  environment: environmentMap[process.env.ENVIRONMENT],
   merchantId: process.env.MERCHANT_ID,
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY
