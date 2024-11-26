3134403c19cb3aedbda36c9d9383406b4d77a50d
RickeySerna <rickeyserna7@live.com>
2024-11-26 11:49:57 -0600

    Added error checking for missing .env vars

--- gatewaycreate.js
+++ gatewaycreate.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const braintree = require('braintree');
 require('dotenv').config();
 
+// Defining all of the environment variables needed to run the app.
+const requiredEnvVars = ["ENVIRONMENT", "MERCHANT_ID", "PUBLIC_KEY", "PRIVATE_KEY"];
+// Creating a var filled with any missing environment variable.
+const missingEnvVars = requiredEnvVars.filter(varName => !process.env[varName]);
+
+// Checking if missingEnvVars is has any values which would mean that some environment var or vars is/are missing.
+if (missingEnvVars.length > 0) {
+  // If so, throw an error mentioning the missing vars.
+  console.error(`The following environment variables are missing or empty: ${missingEnvVars.join(', ')}. Please set them in your .env file.`);
+  process.exit(1);
+}
+
 const environmentMap = {
   'Sandbox': braintree.Environment.Sandbox,
   'Production': braintree.Environment.Production
